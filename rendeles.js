f8c63531aca5ece0b924af24b3dd657aee216f55
horvathbence7273 <horvath.bence7273@gmail.com>
2023-10-19 19:31:15 +0200

    Rendeles befeezve

--- rendeles.js
+++ rendeles.js
@@ -1,26 +1,26 @@
-function createBill() {
+
+function calculateTotalPrice() {
+  const gomolysajt = parseFloat(document.getElementById("gomolysajt").value) * 2790;
+  const jercemell = parseFloat(document.getElementById("jercemell").value) * 3190;
+  const borjupaprikas = parseFloat(document.getElementById("borjupaprikas").value) * 4590;
+  const libamajaspulykamell = parseFloat(document.getElementById("libamajaspulykamell").value) * 4690;
+  const sultoldalas = parseFloat(document.getElementById("sultoldalas").value) * 4190;
+  const zoldsegesozragu = parseFloat(document.getElementById("zoldsegesozragu").value) * 5590;
+
+  const totalPrice = gomolysajt + jercemell + borjupaprikas + libamajaspulykamell + sultoldalas + zoldsegesozragu;
+
+  document.getElementById("totalPrice").textContent = totalPrice;
+}
 
 
-    var name = document.getElementById('name');
-    var address = document.getElementById('address');
-    var email = document.getElementById('address');
-    var mobile = document.getElementById('postcode');
+const inputFields = document.querySelectorAll('input[type="number"]');
+inputFields.forEach(function (input) {
+  input.addEventListener("input", calculateTotalPrice);
+});
+calculateTotalPrice();
+
+function rendel(){
   
-    if (name.value == "") {
-      alert("A 'Teljes név' mezőt ne hagyd üresen!");
-      return false;
-    }
+    alert("Köszönjük a rendelést!");
   
-    if (address.value == "") {
-      alert("A 'Cím' mezőt ne hagyd üresen!");
-      return false;
-    }
-    if (email.value == "") {
-      alert("Az 'E-mail' mezőt ne hagyd üresen!");
-      return false;
-    }
-    if (mobile.value == "") {
-      alert("A 'Telefonszám' mezőt ne hagyd üresen!");
-      return false;
-    }
 }
